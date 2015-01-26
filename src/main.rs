d9dde4eb941eb8ac93cdbafd7afd98ecec19c71e
Patrick Lambein <patrick@lambein.name>
2015-01-26 22:57:10 +0100

    Create trait BEncodable, impl for i64.

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,25 @@
-fn to_benc (str: &str) -> String {
-    let l1 = str.len();
-    let tmp = format!("{}:", l1);
-    let l2 = tmp.len();
+trait BEncodable {
+    fn to_benc(self) -> String;
+}
 
-    let mut s = String::with_capacity(l1 + l2);
-    s.push_str(tmp.as_slice());
-    s.push_str(str.as_slice());
+impl<'a> BEncodable for &'a str {
+    fn to_benc (self) -> String {
+        let l1 = self.len();
+        let tmp = format!("{}:", l1);
+        let l2 = tmp.len();
 
-    return s;
+        let mut s = String::with_capacity(l1 + l2);
+        s.push_str(tmp.as_slice());
+        s.push_str(self);
+
+        return s;
+    }
+}
+
+impl BEncodable for i64 {
+    fn to_benc (self) -> String {
+        return format!("i{}e", self);
+    }
 }
 
 fn main() {
@@ -16,11 +28,19 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use super::to_benc;
+    use super::BEncodable;
 
-    #[test]
-    fn test_to_benc() {
-        assert_eq!("0:", to_benc(""));
-        assert_eq!("4:test", to_benc("test"));
+#[test]
+    fn test_str_to_benc() {
+        assert_eq!("0:", "".to_benc());
+        assert_eq!("4:test", "test".to_benc());
+    }
+
+#[test]
+    fn test_i64_to_benc() {
+        assert_eq!("i0e", 0.to_benc());
+        assert_eq!("i1e", 1.to_benc());
+        assert_eq!("i1000e", 1000.to_benc());
+        assert_eq!("i-1e", (-1).to_benc());
     }
 }
